14c1c1f0c5932ceb4194561f0f95349a266f1997
happydev1237 <alexanderdavis0305@gmail.com>
2023-03-26 14:54:50 -0500

    Updates

---
+++ precache-manifest.e481346fe9d576498312a53ca1c60813.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c27a5fc1987abc35495177361775c37f",
+    "url": "/github.io/portfolio-AlexanderDavis/index.html"
+  },
+  {
+    "revision": "d9441697677c6307e6ad",
+    "url": "/github.io/portfolio-AlexanderDavis/static/css/main.f2d12ebb.chunk.css"
+  },
+  {
+    "revision": "71a0dee73e3f33124c39",
+    "url": "/github.io/portfolio-AlexanderDavis/static/js/2.6f1737b1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github.io/portfolio-AlexanderDavis/static/js/2.6f1737b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9441697677c6307e6ad",
+    "url": "/github.io/portfolio-AlexanderDavis/static/js/main.d412476a.chunk.js"
+  },
+  {
+    "revision": "f74dd7eadccef65c3f99",
+    "url": "/github.io/portfolio-AlexanderDavis/static/js/runtime-main.9fd1f3d8.js"
+  },
+  {
+    "revision": "f3afea6e3784d78af14654fce4a31e19",
+    "url": "/github.io/portfolio-AlexanderDavis/static/media/code.f3afea6e.svg"
+  },
+  {
+    "revision": "8f53edd1344877333c4e84527f3b0af3",
+    "url": "/github.io/portfolio-AlexanderDavis/static/media/hexlogo.8f53edd1.svg"
+  },
+  {
+    "revision": "0110e78057628101288cb406ea62d6c6",
+    "url": "/github.io/portfolio-AlexanderDavis/static/media/me.0110e780.png"
+  },
+  {
+    "revision": "50baad3bf195eff69067bc20fe2b1088",
+    "url": "/github.io/portfolio-AlexanderDavis/static/media/plate.50baad3b.svg"
+  },
+  {
+    "revision": "9542bf91b8920b4d4b706034b795c38e",
+    "url": "/github.io/portfolio-AlexanderDavis/static/media/plato.9542bf91.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/github.io/portfolio-AlexanderDavis/precache-manifest.e481346fe9d576498312a53ca1c60813.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/github.io/portfolio-AlexanderDavis/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
